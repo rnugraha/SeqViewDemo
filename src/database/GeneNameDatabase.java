97bd9cc6ba3ab7cfb979eec37d29520b3a9c05e6
Tuomas Pellonperä <tuomas.pellonpera@gmail.com>
2012-11-16 10:30:05 +0200

    Added a javadoc comment to 'getHgncData'.

--- src/database/GeneNameDatabase.java
+++ src/database/GeneNameDatabase.java
@@ -4,6 +4,12 @@ public interface GeneNameDatabase {
 
     abstract public DatabaseQueryResult getGeneNamesAndSymbols(String filterName);
 
+    /**
+     * Get HGNC (HUGO Gene Nomenclature Committee) data about a gene.
+     * 
+     * @param symbol    approved symbol name of the desired gene
+     * @return          HGNC data 
+     */
     abstract public HgncData getHgncData(String symbol);
 
 }
