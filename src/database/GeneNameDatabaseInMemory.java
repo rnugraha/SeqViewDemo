e36b9668e7c37236846a0fdf761bebc895aa979d
Tuomas Pellonperä <tuomas.pellonpera@gmail.com>
2012-11-16 11:28:58 +0200

    Added a javadoc comment.

--- src/database/GeneNameDatabaseInMemory.java
+++ src/database/GeneNameDatabaseInMemory.java
@@ -1,5 +1,18 @@
 package database;
 
+/**
+ * This class implements an in-memory database for gene names.
+ * <p>The idea is to give users a chance to use a 
+ * <a href="http://www.json.org/">JSON</a> file of their own 
+ * and feed it into the database engine. All this without 
+ * any extensive application configuration.</p>
+ * <p>The database engine used is 
+ * <a href="http://www.orientdb.org/">OrientDB</a>.</p>
+ * 
+ * @author Tuomas Pellonperä
+ *
+ */
+
 public class GeneNameDatabaseInMemory implements GeneNameDatabase {
 
     @Override
