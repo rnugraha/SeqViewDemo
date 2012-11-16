ad27139e6fd43aee4d1c332ba89ae833aac54661
Tuomas Pellonperä <tuomas.pellonpera@gmail.com>
2012-11-16 11:23:21 +0200

    Created a database class which holds gene names in memory.

---
+++ src/database/GeneNameDatabaseInMemory.java
@@ -0,0 +1,17 @@
+package database;
+
+public class GeneNameDatabaseInMemory implements GeneNameDatabase {
+
+    @Override
+    public DatabaseQueryResult getGeneNamesAndSymbols(String filterName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public HgncData getHgncData(String symbol) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
