154912004f97973d10f1ec4f778830de045ad89c
Tuomas Pellonperä <tuomas.pellonpera@gmail.com>
2012-11-16 12:43:57 +0200

    Added constructors and fields.

--- src/database/GeneNameDatabaseInMemory.java
+++ src/database/GeneNameDatabaseInMemory.java
@@ -1,5 +1,7 @@
 package database;
 
+import java.io.File;
+
 /**
  * This class implements an in-memory database for gene names.
  * <p>The idea is to give users a chance to use a 
@@ -14,6 +16,21 @@ package database;
  */
 
 public class GeneNameDatabaseInMemory implements GeneNameDatabase {
+    
+    public GeneNameDatabaseInMemory() {
+        this(DEFAULT_HGNC_DATA);
+    }
+
+    public GeneNameDatabaseInMemory(DatabaseConfig conf) {
+        this(DEFAULT_HGNC_DATA);
+    }
+
+    public GeneNameDatabaseInMemory(String src) {
+        source = new File(src);
+        if (source.isDirectory() || !source.exists()) {
+            source = new File(DEFAULT_HGNC_DATA);
+        }
+    }
 
     @Override
     public DatabaseQueryResult getGeneNamesAndSymbols(String filterName) {
@@ -23,8 +40,21 @@ public class GeneNameDatabaseInMemory implements GeneNameDatabase {
 
     @Override
     public HgncData getHgncData(String symbol) {
-        // TODO Auto-generated method stub
+        
         return null;
     }
+    
+    private static String abspath(String filename) {
+        return HOMEDIR.concat(SEPARATOR).concat(filename);
+    }
 
+    private static final String HOMEDIR = System.getProperty("user.dir");
+    private static final String SEPARATOR = System.getProperty("file.separator");
+    private static final String DEFAULT_HGNC_DATA;
+    
+    private File source;
+    
+    static {
+        DEFAULT_HGNC_DATA = abspath("genedata/hgnc_genes.json");
+    }
 }
