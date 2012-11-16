01f24f630c355ff50005dc765746ab72e4e20fb2
Tuomas Pellonperä <tuomas.pellonpera@gmail.com>
2012-11-16 10:41:32 +0200

    Added a javadoc comment to 'getGeneNamesAndSymbols'.

--- src/database/GeneNameDatabase.java
+++ src/database/GeneNameDatabase.java
@@ -2,6 +2,14 @@ package database;
 
 public interface GeneNameDatabase {
 
+    /**
+     * Search HGNC data using a filter and return all matching 
+     * gene names and symbols.
+     * 
+     * @param filterName    Matches both approved symbol name and approved name.
+     * @return              An object containing two ArrayList<String> objects
+     *                      (one for approved symbols, one for approved names)
+     */
     abstract public DatabaseQueryResult getGeneNamesAndSymbols(String filterName);
 
     /**
