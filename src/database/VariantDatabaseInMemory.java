5fee52d4a4fe81a21ec7a2877e35bccbe6eeaeca
Tuomas Pellonperä <tuomas.pellonpera@gmail.com>
2012-11-20 09:48:50 +0200

    Created the 'VariantDatabaseInMemory' class.

---
+++ src/database/VariantDatabaseInMemory.java
@@ -0,0 +1,29 @@
+package database;
+
+public class VariantDatabaseInMemory implements VariantDatabase {
+
+    @Override
+    public DatabaseQueryResult getHgncData(String gene) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public DatabaseQueryResult getVariantById(String id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public DatabaseQueryResult getVariantsData(String gene, int skip, int limit) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setGeneNameDatabase(GeneNameDatabase geneDb) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
