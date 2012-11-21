1fbcd2299b9f137d5ac76426d8b9d7d3f289091c
Tuomas Pellonperä <tuomas.pellonpera@gmail.com>
2012-11-21 09:10:58 +0200

    First import.

---
+++ src/security/SqlParameter.java
@@ -0,0 +1,7 @@
+package security;
+
+public class SqlParameter {
+    public static final int UPPERCASE           = 1;
+    public static final int LOWERCASE           = 2;
+    public static final int NUMBERS             = 4;
+}
